d2b79a76ddf35fb41742d71f6bb4742352b44391
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-03-27 23:49:55 +0800

    添加项目文件。

---
+++ MasterChief.DotNet.Infrastructure.SerializerSample/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ProtoBuf;
+
+namespace MasterChief.DotNet.Infrastructure.SerializerSample
+{
+    [Serializable]
+    [ProtoContract]
+    public sealed class Person
+    {
+        /// <summary>
+        /// 使用protobuf的时候，默认构造函数必须有，反序列化会报 No parameterless constructor found for x 错误
+        /// </summary>
+        public Person()
+        {
+        }
+        [ProtoMember(1)]
+        public string FirstName { get; set; }
+        [ProtoMember(2)]
+        public string LastName { get; set; }
+        [ProtoMember(3)]
+        public int Age { get; set; }
+        [ProtoMember(4)]
+        public string Remark { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"FirstName:{FirstName},LastName:{LastName},Age:{Age},Remark:{Remark}";
+        }
+    }
+}
